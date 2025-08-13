3b332c8199c1a7a7f1a54418a2871267ad4fb591
Felipe Dutra <dutrafelipe77@gmail.com>
2025-08-13 10:59:57 -0300

    preparing development

--- backend/run.py
+++ backend/run.py
@@ -2,8 +2,3 @@ from app import create_app
 
 
 app = create_app()
-
-if __name__ == '__main__':
-    # Esta linha inicia o servidor e o faz escutar em todas as interfaces de rede
-    # o que é necessário para o Docker
-    app.run(host='0.0.0.0', port=5000)
